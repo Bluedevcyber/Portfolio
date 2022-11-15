4d8b9f0e228434318fb833d48cf7b77824cbfecf
Geoffrey <geoffrey.wavelet@yahoo.com>
2022-11-15 15:08:39 +0100

    ajout portfolio

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+// pour gérer les effets et appeler la bibliothèque Locomotive
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    tablet: { smooth: true},
+    smartphone: { smooth: true},
+});
+
+// Body Color
+scroll.on("scroll", () => {
+    if (document.querySelector("#color.is-inview")) {
+        document.body.style.background = "#000101";
+        document.body.style.color = "#fefeff";
+    } else {
+        document.body.style.background = "#fefeff";
+        document.body.style.color = "#000101";
+    }
+});
